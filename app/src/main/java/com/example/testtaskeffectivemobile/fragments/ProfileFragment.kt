2090fcae277a358f7d479f396aa9e781ffb71f5d
madmax <maksimkholin@gmail.com>
2024-10-20 16:52:45 +0300

    add all fragments

--- app/src/main/java/com/example/testtaskeffectivemobile/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/testtaskeffectivemobile/fragments/ProfileFragment.kt
@@ -1,19 +1,20 @@
-package com.example.testtaskeffectivemobile
+package com.example.testtaskeffectivemobile.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.testtaskeffectivemobile.R
 
-class FavoritesFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorites, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
 }
