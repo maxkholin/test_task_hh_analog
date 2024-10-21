5f1c80cbb6c81798fe6148b96547e3ce08f72bbe
madmax <maksimkholin@gmail.com>
2024-10-21 16:20:13 +0300

    переместил файлы

--- app/src/main/java/com/example/testtaskeffectivemobile/ui/fragments/FavoritesFragment.kt
+++ app/src/main/java/com/example/testtaskeffectivemobile/ui/fragments/FavoritesFragment.kt
@@ -1,4 +1,4 @@
-package com.example.testtaskeffectivemobile.fragments
+package com.example.testtaskeffectivemobile.ui.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
--- app/src/main/java/com/example/testtaskeffectivemobile/ui/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/testtaskeffectivemobile/ui/fragments/SearchFragment.kt
@@ -1,20 +1,20 @@
-package com.example.testtaskeffectivemobile.fragments
+package com.example.testtaskeffectivemobile.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.testtaskeffectivemobile.R
 
-class MessagesFragment : Fragment() {
+class SearchFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_messages, container, false)
+        return inflater.inflate(R.layout.fragment_search, container, false)
     }
-
 }
