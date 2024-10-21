e5c3f30008551b44b99f7ddeff81fddcd834e8a5
madmax <maksimkholin@gmail.com>
2024-10-21 14:31:07 +0300

    добавил переключение экранов - пока не работает

--- app/src/main/java/com/example/testtaskeffectivemobile/MainActivity.kt
+++ app/src/main/java/com/example/testtaskeffectivemobile/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.testtaskeffectivemobile
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,12 +26,27 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        initAndSetupNavigation()
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+
+    }
+
+    private fun initAndSetupNavigation() {
+        bottomNavigationView = findViewById(R.id.bottom_nav)
         bottomNavigationView.setOnApplyWindowInsetsListener { v, insets ->
             v.setPadding(0, 0, 0, 0)
             insets
         }
+
+//        navController = try {
+//            findNavController(R.id.nav_host_fragment)
+//        } catch (e: Exception) {
+//            Log.e("NavigationSetup", "NavController initialization failed: ${e.message}")
+//            throw e
+//        }
+//        // Настройка NavController с BottomNavigationView
+//        bottomNavigationView.setupWithNavController(navController)
     }
 
 
