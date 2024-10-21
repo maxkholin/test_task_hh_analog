616519662e613a09ec37a12e8502138fcc1578c3
madmax <maksimkholin@gmail.com>
2024-10-21 23:13:47 +0300

    добавил адаптер для массива Offers

--- app/src/main/java/com/example/testtaskeffectivemobile/data/model/Offer.kt
+++ app/src/main/java/com/example/testtaskeffectivemobile/data/model/Offer.kt
@@ -2,13 +2,13 @@ package com.example.testtaskeffectivemobile.data.model
 
 import com.google.gson.annotations.SerializedName
 
-data class Offers(
-    @SerializedName("id") val id: String?,
+data class Offer(
+    @SerializedName("id") val imageId: String?,
     @SerializedName("title") val title: String?,
     @SerializedName("button") val button: Button?,
     @SerializedName("link") val link: String?
 )
 
 data class Button(
-    @SerializedName("button") val text: String?
+    @SerializedName("text") val text: String?
 )
