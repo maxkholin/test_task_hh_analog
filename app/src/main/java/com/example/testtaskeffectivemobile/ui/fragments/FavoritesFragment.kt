3531b818d85e240c05981e04f3d091b9728ec46c
madmax <maksimkholin@gmail.com>
2024-10-23 02:04:32 +0300

    make refactoring: улучшил структуру кода

--- app/src/main/java/com/example/testtaskeffectivemobile/ui/fragments/FavoritesFragment.kt
+++ app/src/main/java/com/example/testtaskeffectivemobile/ui/fragments/FavoritesFragment.kt
@@ -16,9 +16,7 @@ class FavoritesFragment : Fragment() {
 
     private lateinit var vacancyRecyclerView: RecyclerView
     private lateinit var vacancyAdapter: VacancyAdapter
-
     private lateinit var mainViewModel: MainViewModel
-
     private lateinit var favoriteVacanciesCount: TextView
 
     override fun onCreateView(
@@ -31,22 +29,30 @@ class FavoritesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Инициализация ViewModel
         mainViewModel = ViewModelProvider(requireActivity())[MainViewModel::class.java]
 
-        // Настройка RecyclerView для вакансий
+        setupRecyclerView(view)
+        setupFavoriteVacanciesCount(view)
+        observeFavoriteVacancies()
+    }
+
+    /** Настраивает RecyclerView для отображения избранных вакансий. */
+    private fun setupRecyclerView(view: View) {
         vacancyRecyclerView = view.findViewById(R.id.vacanciesRecyclerView)
         vacancyAdapter = VacancyAdapter(mainViewModel)
         vacancyRecyclerView.adapter = vacancyAdapter
+    }
 
-        // Инициализация, установка текста кнопки
+    /** Настраивает отображение количества избранных вакансий. */
+    private fun setupFavoriteVacanciesCount(view: View) {
         favoriteVacanciesCount = view.findViewById(R.id.favoriteVacanciesCount)
-
         mainViewModel.favoriteVacanciesCount.observe(viewLifecycleOwner) { text ->
             favoriteVacanciesCount.text = text
         }
+    }
 
-        // Подписка на обновления списка избранных вакансий
+    /** Подписка на обновления списка избранных вакансий. */
+    private fun observeFavoriteVacancies() {
         mainViewModel.favoriteVacancies.observe(viewLifecycleOwner) { favoriteVacancies ->
             vacancyAdapter.vacancies = favoriteVacancies
         }
