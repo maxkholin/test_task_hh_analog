b05f56b70d2ab34b35100b4377511d8d3dcd81d2
madmax <maksimkholin@gmail.com>
2024-10-23 01:36:53 +0300

    checkpoint remove fields

--- app/src/main/java/com/example/testtaskeffectivemobile/data/model/Vacancy.kt
+++ app/src/main/java/com/example/testtaskeffectivemobile/data/model/Vacancy.kt
@@ -1,22 +1,30 @@
 package com.example.testtaskeffectivemobile.data.model
 
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity(tableName = "favorite_vacancies")
 data class Vacancy(
-    @SerializedName("id") val id: String?,
+    @PrimaryKey
+    @SerializedName("id") val id: String,
     @SerializedName("lookingNumber") val lookingNumber: Int?,
     @SerializedName("title") val title: String?,
+    @Embedded
     @SerializedName("address") val address: Address?,
     @SerializedName("company") val company: String?,
+    @Embedded
     @SerializedName("experience") val experience: Experience?,
     @SerializedName("publishedDate") val publishedDate: String?,
     @SerializedName("isFavorite") var isFavorite: Boolean,
+    @Embedded
     @SerializedName("salary") val salary: Salary?,
-    @SerializedName("schedules") val schedules: List<String>,
+    // @SerializedName("schedules") val schedules: List<String>,
     @SerializedName("appliedNumber") val appliedNumber: Int?,
     @SerializedName("description") val description: String?,
     @SerializedName("responsibilities") val responsibilities: String?,
-    @SerializedName("questions") val questions: List<String>
+    // @SerializedName("questions") val questions: List<String>
 )
 
 data class Address(
